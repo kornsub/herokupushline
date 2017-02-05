ca1aa90b0ab8a2da6db7d32349d1224116ad2662
kornsub <kornsub.s@gmail.com>
2017-02-05 19:09:30 +0700

    Add files via upload

--- push.php
+++ push.php
@@ -1,27 +1,47 @@
 <?php 
 $access_token = "RNiI64Nz6IIxxyyD0hc83r1gNUHEIHONHa3no3s0Eo+xMs2ftaA9d758TQE6Iy06guYt+yo3x5Fhb3ETuWyjmtG3JLGppnC/H3ZamCLLtvTpcew+D9f39TEbPvEVOuV4WFxeH13sk4okTFsTHHCjEwdB04t89/1O/w1cDnyilFU=";
-$headers = array( 'Content-type: application/json', 'Authorization: Bearer'.$access_token); 
-$messages = ['type' => 'text','text' => 'Hi'];
-$data = ['replyToken' => $replyToken,'messages' => [$messages],];
-$post = json_encode($data);
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-$chOne = curl_init(); 
-curl_setopt( $chOne, CURLOPT_URL, "https://api.line.me/v2/bot/message/push"); 
-curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt( $chOne, CURLOPT_POSTFIELDS, $post);
-curl_setopt( $chOne, CURLOPT_HTTPHEADER, $headers);
-curl_setopt( $chOne, CURLOPT_FOLLOWLOCATION, 1); 
-curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
-curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt( $chOne, CURLOPT_POST, 1); 
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-$result = curl_exec( $chOne ); 
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-//Check error 
-if(curl_error($chOne)) { echo 'error:' . curl_error($chOne); } 
-else { $result_ = json_decode($result, true); 
-echo "status : ".$result_['status']; echo "message : ". $result_['message']; } 
-
-curl_close( $chOne ); 
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
 ?>
