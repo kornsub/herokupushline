a9facc324481f1511e7f4d6d761623d423fde447
kornsub <kornsub.s@gmail.com>
2018-06-16 10:22:58 +0700

    Update reply.php

--- reply.php
+++ reply.php
@@ -1,6 +1,7 @@
 <?php 
 //$access_token = "0iUcdcZOrWfzqmAsJvL+4w71v+DKgg+a+QKnZZuu4kO02zRaG03ei2FS1VUgz5M8C0ip/IwOQC7HQMZTHche5CpJUWaxvHjnVH9/uCNcmgvxBbBmLU4zLZ9fg1CQbRCdXPxWN/zGwm48V83918r2kwdB04t89/1O/w1cDnyilFU=";
-$access_token = "Q5JtYUSsKcDOw2qt1WBLFe7pAv+lq1HH1TpkokYfhc5SSk9fUOJxh1ijHkNDq5SKiPjQRsbtrujAG4YyCZK4zecc1a8cfUvJ2bHnDrZ0GBN25DRFJFd7SPJ94CpK7lquMxeGhLAL79jfEpWYh+gPMAdB04t89/1O/w1cDnyilFU=";
+//$access_token = "Q5JtYUSsKcDOw2qt1WBLFe7pAv+lq1HH1TpkokYfhc5SSk9fUOJxh1ijHkNDq5SKiPjQRsbtrujAG4YyCZK4zecc1a8cfUvJ2bHnDrZ0GBN25DRFJFd7SPJ94CpK7lquMxeGhLAL79jfEpWYh+gPMAdB04t89/1O/w1cDnyilFU=";
+$access_token = "XOoLBLmegCRH0kfZBJkNBn/52/0yDWk8C2FNCDbA7pYVYuAtNhHdxhWhrAWi+fS7VTvJtAWrYZNVLe1V1ikZ+9NRWXc9u6HBgf/RBrmmXkpmkeDe8fzB3Jd9bHUKNrq8R+l9opobVBM0tH1iw5ZTtwdB04t89/1O/w1cDnyilFU=";
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
