ef450f114782ab20e41d3156b27382001912d5da
kornsub <kornsub.s@gmail.com>
2018-06-16 10:26:36 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php 
-$access_token = "RNiI64Nz6IIxxyyD0hc83r1gNUHEIHONHa3no3s0Eo+xMs2ftaA9d758TQE6Iy06guYt+yo3x5Fhb3ETuWyjmtG3JLGppnC/H3ZamCLLtvTpcew+D9f39TEbPvEVOuV4WFxeH13sk4okTFsTHHCjEwdB04t89/1O/w1cDnyilFU=";
-
+//$access_token = "RNiI64Nz6IIxxyyD0hc83r1gNUHEIHONHa3no3s0Eo+xMs2ftaA9d758TQE6Iy06guYt+yo3x5Fhb3ETuWyjmtG3JLGppnC/H3ZamCLLtvTpcew+D9f39TEbPvEVOuV4WFxeH13sk4okTFsTHHCjEwdB04t89/1O/w1cDnyilFU=";
+$access_token = "XOoLBLmegCRH0kfZBJkNBn/52/0yDWk8C2FNCDbA7pYVYuAtNhHdxhWhrAWi+fS7VTvJtAWrYZNVLe1V1ikZ+9NRWXc9u6HBgf/RBrmmXkpmkeDe8fzB3Jd9bHUKNrq8R+l9opobVBM0tH1iw5ZTtwdB04t89/1O/w1cDnyilFU=";
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
